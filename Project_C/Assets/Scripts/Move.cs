a40c0eb72f88c9005dc6501fe29a6f702fd2df3b
NagasawaS <terumo0520@yahoo.co.jp>
2016-05-17 15:25:39 +0900

    タイトルとパーティー編成
    画像乗っけたよ

---
+++ Project_C/Assets/Scripts/Move.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class Move : MonoBehaviour {
+
+    private int i;
+    private GameObject No;
+
+	// Use this for initialization
+	void Start () {
+        i = 0;
+        No = GameObject.Find("No");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        if (Input.GetKey(KeyCode.RightArrow)) {
+        }
+	}
+}
