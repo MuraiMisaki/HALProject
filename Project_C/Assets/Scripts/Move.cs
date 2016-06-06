419a0088a70c073d602c37aec113ffb3ebb0e6c1
NagasawaS <terumo0520@yahoo.co.jp>
2016-06-06 16:24:20 +0900

    パーティー編成変更しました
    大まかなパーティー編成の動きが完成した

--- Project_C/Assets/Scripts/Move.cs
+++ Project_C/Assets/Scripts/Move.cs
@@ -1,47 +1,57 @@
 ﻿using UnityEngine;
-using UnityEngine.UI;
 using System.Collections;
 
 public class Move : MonoBehaviour {
 
-    private GameObject No;
+    private GameObject cursor;
 
 	// Use this for initialization
 	void Start () {
-        No = GameObject.Find("No");
+        cursor = GameObject.Find("cursor");
 	}
 	
 	// Update is called once per frame
 	void Update () {
 
-        if (Input.GetKeyDown(KeyCode.RightArrow))
-        {
-            foreach (Transform child in transform)
-            {
-                //child is your child transform
-                child.transform.localPosition += new Vector3(-140, 0, 0);
+        if (cursor.transform.localPosition.y < 0) {
 
-                if (child.transform.localPosition.x < -840)
-                {
-                    child.transform.localPosition += new Vector3(1680, 0, 0);
+            if (Input.GetKeyDown(KeyCode.RightArrow)) {
+
+                foreach (Transform child in transform) {
+            
+                    child.transform.localPosition += new Vector3(-140, 0, 0);
+
+                    if (child.transform.localPosition.x < -840) {
+                
+                        child.transform.localPosition += new Vector3(1820, 0, 0);
+                    }
                 }
             }
-        }
         
+            if (Input.GetKeyDown(KeyCode.LeftArrow)) {
+        
+                foreach (Transform child in transform) {
 
-        if (Input.GetKeyDown(KeyCode.LeftArrow))
-        {
-            foreach (Transform child in transform)
-            {
-                //child is your child transform
-                child.transform.localPosition += new Vector3(140, 0, 0);
+                    child.transform.localPosition += new Vector3(140, 0, 0);
 
-                if (child.transform.localPosition.x > 840)
-                {
-                    child.transform.localPosition += new Vector3(-1680, 0, 0);
+                    if (child.transform.localPosition.x > 840) {
+
+                        child.transform.localPosition += new Vector3(-1820, 0, 0);
+                    }
                 }
-
             }
         }
+
+        foreach (Transform child in transform) {
+
+            if (child.transform.localPosition.x < -280 || child.transform.localPosition.x > 280) {
+                child.gameObject.SetActive(false);
+            } else {
+                child.gameObject.SetActive(true);
+            }
+        }
+ 
 	}
+
+
 }
---
+++ Project_C/Assets/Scripts/MoveCursor.cs
@@ -0,0 +1,76 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MoveCursor : MonoBehaviour {
+
+    private GameObject No;
+    GameObject Manager;
+    
+	// Use this for initialization
+	void Start () {
+        Manager = GameObject.Find("Manager");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        if (Input.GetKeyDown(KeyCode.DownArrow)) {
+
+            this.transform.localPosition = new Vector3(0, -30, 0);
+        }
+
+        if (this.transform.localPosition.y > 0) {
+
+            if (Input.GetKeyDown(KeyCode.RightArrow))
+            {
+                if (this.transform.localPosition.x < 200)
+                {
+                    this.transform.localPosition += new Vector3(140, 0, 0);
+                }
+                else {
+                    this.transform.localPosition = new Vector3(340, 85, 0);
+                }
+
+            }
+
+            if (Input.GetKeyDown(KeyCode.LeftArrow)) {
+                if (this.transform.localPosition.x < 210)
+                {
+                    if (this.transform.localPosition.x > -210)
+                        this.transform.localPosition += new Vector3(-140, 0, 0);
+                }
+                else {
+                    this.transform.localPosition = new Vector3(210, 120, 0);
+                }
+
+            }
+
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                TexManager Ma = Manager.GetComponent<TexManager>();
+                if (this.transform.localPosition.x < 250)
+                    Ma.Setdef(No);
+            }
+ 
+        
+        }
+        else {
+            if (Input.GetKeyDown(KeyCode.UpArrow)) {
+
+                this.transform.localPosition = new Vector3(-210, 120, 0);
+            }
+
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                    TexManager Ma = Manager.GetComponent<TexManager>();
+                    Ma.GetTex(No);
+            }
+        }
+
+    }
+
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        No = collision.gameObject;
+    }
+}
